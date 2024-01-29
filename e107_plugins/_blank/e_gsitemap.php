1fbf21654e3588b7a660154b27c75bfb369a4c06
Jimako <janamicek@gmail.com>
2024-01-29 17:03:30 +0100

    missing addons from plugin builder

---
+++ e107_plugins/_blank/e_gsitemap.php
@@ -0,0 +1,35 @@
+<?php
+/*
+* e107 website system
+*
+* Copyright (C) 2008-2018 e107 Inc (e107.org)
+* Released under the terms and conditions of the
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+*
+*	gSitemap addon
+*/
+
+if (!defined('e107_INIT'))
+{
+	exit;
+}
+ 
+
+class _blank_gsitemap
+{
+
+	// Dynamically Generated Sitemap;
+	function config()
+	{
+	
+
+	}
+
+
+	function import()
+	{
+		 
+
+	}
+
+}
